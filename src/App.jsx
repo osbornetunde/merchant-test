f3ac5037a31d9323dd50a9de534afb8329d906b8
osbornetunde <osbornetunde@gmail.com>
2021-07-20 18:29:13 +0100

    implemented the inline-payment

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom';
 import Payment from "./Payment";
 import SuccessPage from "./pages/SuccessPage";
-import LandingPage from "./pages/LandingPage";
 
 function App() {
 
 
   return (
     <Switch>
-        <Route exact path={'/'} component={LandingPage} />
-        <Route exact path="/standard-payment" component={Payment}/>
+        <Route exact path="/" component={Payment}/>
         <Route path="/successful" component={SuccessPage}/>
     </Switch>
   )
