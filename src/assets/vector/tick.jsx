1573ae18dcdb8a8cf7ff5b93453d4bb7d40ab477
osbornetunde <osbornetunde@gmail.com>
2021-07-21 12:23:09 +0100

    -updated the success page

---
+++ src/assets/vector/tick.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Tick = () => {
+    return (
+        <svg width="99" height="99" viewBox="0 0 99 99" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d)">
+                <path d="M49.1623 76.6621C68.0295 76.6621 83.3245 61.3672 83.3245 42.4999C83.3245 23.6326 68.0295 8.33765 49.1623 8.33765C30.295 8.33765 15 23.6326 15 42.4999C15 61.3672 30.295 76.6621 49.1623 76.6621Z" fill="#3DB39E"/>
+            </g>
+            <path d="M81.6471 31.9248C80.6796 28.9506 79.3186 26.1561 77.6208 23.6028L47.7172 54.7491L47.7774 58.2153L49.189 58.2063L81.6471 31.9248Z" fill="#37A18E"/>
+            <path d="M83.3763 21.7853L77.2845 15.7666C76.453 14.9447 75.1043 14.9447 74.2728 15.7666L48.2323 42.5894L37.0025 31.4935C36.1697 30.6716 34.8223 30.6716 33.9901 31.4935L28.5905 36.8289C27.759 37.6502 27.759 38.9825 28.5905 39.8037L46.6137 57.6097C47.094 58.0845 47.7452 58.2759 48.3711 58.2021C48.997 58.2752 49.648 58.0845 50.1285 57.6097L83.3765 24.7607C84.2078 23.9388 84.2078 22.6072 83.3763 21.7853Z" fill="#F8F8F8"/>
+            <path d="M50.1284 57.6101L83.3764 24.7611C84.2079 23.9392 84.2079 22.6076 83.3764 21.7855L82.3823 20.803L48.1812 54.4426L29.3455 36.0824L28.5912 36.8285C27.7597 37.6498 27.7597 38.9821 28.5912 39.8033L46.6137 57.6101C47.094 58.0849 47.7452 58.2763 48.371 58.2025C48.9969 58.2763 49.648 58.0857 50.1284 57.6101Z" fill="#EBEBEB"/>
+            <defs>
+                <filter id="filter0_d" x="0" y="0.337646" width="98.3245" height="98.3245" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+                    <feOffset dy="7"/>
+                    <feGaussianBlur stdDeviation="7.5"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.2 0 0 0 0 0.6 0 0 0 0 0.528814 0 0 0 0.35 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+                </filter>
+            </defs>
+        </svg>
+    );
+};
+
+export default Tick;
--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,10 +1,87 @@
-import React from 'react';
-import {Box, Container} from "@chakra-ui/react";
+import React, {useEffect, useState} from 'react';
+import { useHistory } from 'react-router-dom'
+import {Box, Container, Text, Button} from "@chakra-ui/react";
+import Tick from '../assets/vector/tick'
 
 const SuccessPage = () => {
+    const history = useHistory()
+    const [currentTime, setCurrentTime] = useState(5)
+
+    useEffect(()=> {
+        const counter = setTimeout(()=>{
+            if(currentTime > 0) {
+                setCurrentTime(prevState => prevState - 1)
+            }
+        }, 1000)
+        return () => clearTimeout(counter)
+    })
+
+    useEffect(() => {
+        if(currentTime === 0){
+            redirectToHomePage()
+        }
+
+    },[currentTime])
+
+    const redirectToHomePage = () => history.push('/')
     return (
-        <Container>
-            <Box w="100%" d="flex" justifyContent="center" alignItems="center" fontSize="3rem">Your Payment was successful</Box>
+        <Container
+            maxW="100%"
+            w="100%"
+            position={"relative"}
+            padding={0}
+            maxH="100%"
+            h="100%"
+            margin="0"
+            d={'flex'}
+            justifyContent="center"
+            alignItems='center'
+        >
+            <Box
+                w="24rem"
+                h="17rem"
+                d="flex"
+                borderRadius='1.25rem'
+                justifyContent="center"
+                alignItems="center"
+                flexDir="column"
+                pt='2rem'
+                pb='2.3rem'
+                boxShadow='lg'
+            >
+                <Box>
+                    <Tick/>
+                </Box>
+                <Text
+                    fontSize='1.4rem'
+                    lineHeight='1.65rem'
+                    fontWeight={400}
+                    color='#353542'
+                >
+                    Payment Successful
+                </Text>
+                <Box mt='2.5rem' d='flex'>
+                    <Text
+                        fontSize='0.8rem'
+                        lineHeight='1.4rem'
+                        color='rgba(53, 53, 66, 0.6)'
+                    >
+                        {`Redirecting in  ${currentTime} seconds...`}
+                    </Text>
+                    <Button
+                        ml='0.4rem'
+                        w='2.7rem'
+                        h='1.3rem'
+                        fontSize='0.8rem'
+                        lineHeight='0.9rem'
+                        color='rgba(53, 53, 66, 0.6)'
+                        onClick={redirectToHomePage}
+                    >
+                        Skip
+                    </Button>
+                </Box>
+
+            </Box>
         </Container>
     );
 };
