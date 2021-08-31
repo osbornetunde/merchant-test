ac067b34524635392c6870ba96befd0cde7f57a7
osbornetunde <osbornetunde@gmail.com>
2021-08-31 10:53:09 +0100

    updated baseURL

--- src/utils/httpHelper.js
+++ src/utils/httpHelper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: 'https://api.public.credodemo.com/credo-payment/v1/',
-  baseURL: "https://api.credocentral.com/credo-authentication/v1/users/login",
+  baseURL: "https://api.credocentral.com/credo-payment/v1/",
   headers: {Authorization: import.meta.env.VITE_PUBLIC_KEY},
 });
 
