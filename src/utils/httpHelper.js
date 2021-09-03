2311fcecd5df7f3c31e2f683a6cb3548222bd7c4
osbornetunde <osbornetunde@gmail.com>
2021-09-03 12:56:39 +0100

    changed to nugitech endpoint

--- src/utils/httpHelper.js
+++ src/utils/httpHelper.js
@@ -1,72 +1,98 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.public.credodemo.com/credo-payment/v1/",
+  // baseURL: "https://api.public.credodemo.com/credo-payment/v1/",
+  // headers: {
+  //   Authorization: "pk_demo-85mnMAejb480LTdn0WbG39GdoXuEzJ.nUdQT5I9Pn-d",
+  // },
+  baseURL: "https://credo-payments.nugitech.com/v1/",
   headers: {
-    Authorization: "pk_demo-85mnMAejb480LTdn0WbG39GdoXuEzJ.nUdQT5I9Pn-d",
+    Authorization: "pk_demo-oZkCJkSyFCdb61ZN00nLPVzl5Bdpqp.Yxeb33I11X-d",
   },
-//   baseURL: "https://credo-payments.nugitech.com/v1/",
-//   headers: {
-//     Authorization: "pk_demo-oZkCJkSyFCdb61ZN00nLPVzl5Bdpqp.Yxeb33I11X-d",
-//   },
 });
 
-
-
 const responseHandler = (response) => {
-    const { data, status, ...others } = response;
-    return { data, status, error: false, ...others };
+  const {data, status, ...others} = response;
+  return {data, status, error: false, ...others};
 };
 
 const errorHandler = (response) => {
-    const { data } = response;
-    return data;
+  const {data} = response;
+  return data;
 };
 
-export const buildUrlEncodedParams = (paramsObject) => new URLSearchParams(paramsObject).toString();
+export const buildUrlEncodedParams = (paramsObject) =>
+  new URLSearchParams(paramsObject).toString();
 
-export const makePostRequest = async (requestURL, requestData, configParams = {}, query) => {
-    const params = decodeURIComponent(`${requestURL}${requestData ? `?${buildUrlEncodedParams(requestData)}` : ''}`);
-    try {
-        const data = !query
-            ? await axiosInstance.post(requestURL, requestData, configParams)
-            : await axiosInstance.post(params);
-        return responseHandler(data);
-    } catch (err) {
-        return errorHandler(err.response);
-    }
+export const makePostRequest = async (
+  requestURL,
+  requestData,
+  configParams = {},
+  query
+) => {
+  const params = decodeURIComponent(
+    `${requestURL}${
+      requestData ? `?${buildUrlEncodedParams(requestData)}` : ""
+    }`
+  );
+  try {
+    const data = !query
+      ? await axiosInstance.post(requestURL, requestData, configParams)
+      : await axiosInstance.post(params);
+    return responseHandler(data);
+  } catch (err) {
+    return errorHandler(err.response);
+  }
 };
 
-export const makePutRequest = async (requestURL, requestData, configParams = {}) => {
-    try {
-        const { data } = await axiosInstance.put(requestURL, requestData, configParams);
-        return responseHandler(data);
-    } catch (err) {
-        return errorHandler(err);
-    }
+export const makePutRequest = async (
+  requestURL,
+  requestData,
+  configParams = {}
+) => {
+  try {
+    const {data} = await axiosInstance.put(
+      requestURL,
+      requestData,
+      configParams
+    );
+    return responseHandler(data);
+  } catch (err) {
+    return errorHandler(err);
+  }
 };
-export const makePatchRequest = async (requestURL, requestData, configParams = {}) => {
-    try {
-        const data = await axiosInstance.patch(requestURL, requestData, configParams);
-        return responseHandler(data);
-    } catch (err) {
-        return errorHandler(err);
-    }
+export const makePatchRequest = async (
+  requestURL,
+  requestData,
+  configParams = {}
+) => {
+  try {
+    const data = await axiosInstance.patch(
+      requestURL,
+      requestData,
+      configParams
+    );
+    return responseHandler(data);
+  } catch (err) {
+    return errorHandler(err);
+  }
 };
 
 export const makeGetRequest = async (
-    requestURL,
-    requestData,
-    configParams = {},
+  requestURL,
+  requestData,
+  configParams = {}
 ) => {
-    try {
-        const data = await axiosInstance.get(
-            `${requestURL}${requestData ? `?${buildUrlEncodedParams(requestData)}` : ''}`,
-        );
-        return responseHandler(data);
-    } catch (err) {
-        return errorHandler(err);
-    }
+  try {
+    const data = await axiosInstance.get(
+      `${requestURL}${
+        requestData ? `?${buildUrlEncodedParams(requestData)}` : ""
+      }`
+    );
+    return responseHandler(data);
+  } catch (err) {
+    return errorHandler(err);
+  }
 };
 
 export const makePostRequest2 = async (
