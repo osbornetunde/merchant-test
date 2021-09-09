dcd6c364af292956a324edddb19d5df70ff52414
osbornetunde <osbornetunde@gmail.com>
2021-09-09 12:23:56 +0100

    added www to the script address

--- src/Payment.jsx
+++ src/Payment.jsx
@@ -21,7 +21,7 @@ const paymentOptions = ['CARDS','BANK']
 const currency= 'NGN'
 
 const Payment = () => {
-   useScript("https://credodemo.com/inline.js");
+   useScript("https://www.credodemo.com/inline.js");
 
   const {control, errors, handleSubmit, formState} = useForm({
     mode: "onChange",
